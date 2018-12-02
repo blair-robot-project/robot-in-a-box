1cde276487a1527cdda16233bf68936279d654df
AnahnEmus <justin.hudis@gmail.com>
2018-12-02 11:51:27 -0500

    Read RGB correctly

--- src/main/java/org/usfirst/frc/team449/robot/jacksonWrappers/MappedI2C.java
+++ src/main/java/org/usfirst/frc/team449/robot/jacksonWrappers/MappedI2C.java
@@ -9,6 +9,8 @@ import org.jetbrains.annotations.NotNull;
 import org.usfirst.frc.team449.robot.generalInterfaces.loggable.Loggable;
 import org.usfirst.frc.team449.robot.generalInterfaces.updatable.Updatable;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.Arrays;
 
 /**
@@ -16,20 +18,6 @@ import java.util.Arrays;
  */
 @JsonIdentityInfo(generator = ObjectIdGenerators.StringIdGenerator.class)
 public class MappedI2C implements Loggable, Updatable {
-
-	private final static int TCS34725_ID = 0x12,
-			TCS34725_ATIME = 0x01,
-			TCS34725_INTEGRATIONTIME_50MS = 0xEB,
-			TCS34725_CONTROL = 0x0F,
-			TCS34725_GAIN_4X = 0x01,
-			TCS34725_ENABLE = 0x00,
-			TCS34725_ENABLE_PON = 0x01,
-			TCS34725_ENABLE_AEN = 0x02,
-			TCS34725_ENABLE_AIEN = 0x10,
-			TCS34725_CDATAL = 0x14,
-			TCS34725_RDATAL = 0x16,
-			TCS34725_GDATAL = 0x18,
-			TCS34725_BDATAL = 0x1A;
 
 	/**
 	 * The I2C this class is a wrapper on.
@@ -41,13 +29,6 @@ public class MappedI2C implements Loggable, Updatable {
 	 */
 	private int cachedRed, cachedGreen, cachedBlue;
 
-	private int clear, red, green, blue;
-
-	/**
-	 * For pinging the sensor at specific time intervals.
-	 */
-	private int time;
-
 	/**
 	 * Default constructor.
 	 *
@@ -58,47 +39,22 @@ public class MappedI2C implements Loggable, Updatable {
 	public MappedI2C(@JsonProperty(required = true) I2C.Port port,
 	                 int deviceAddress) {
 		i2c = new I2C(port, deviceAddress);
+	}
 
-		time = -1;
+	public int[] readRGB() {
+		byte[] receivedData = new byte[3];
+		i2c.transaction(new byte[0], 0, receivedData, 3);
 
-		read8(TCS34725_ID);
-		write8(TCS34725_ATIME, TCS34725_INTEGRATIONTIME_50MS);
-		write8(TCS34725_CONTROL, TCS34725_GAIN_4X);
-		write8(TCS34725_ENABLE, TCS34725_ENABLE_PON);
-
-		try {
-			Thread.sleep(3);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		int[] rgb = new int[3];
+		for (int i = 0; i < receivedData.length; i++) {
+			if ((int) receivedData[i] < 0) {
+				rgb[i] = (0x000000FF) & receivedData[i];
+			} else {
+				rgb[i] = (int) receivedData[i];
+			}
 		}
 
-		write8(TCS34725_ENABLE, TCS34725_ENABLE_PON | TCS34725_ENABLE_AEN);
-	}
-
-	public int read8(int registerAddress) {
-		byte[] buffer = new byte[1];
-		i2c.read(registerAddress, 1, buffer);
-		return Byte.toUnsignedInt(buffer[0]);
-	}
-
-	public int read16(int registerAddress) {
-		byte[] buffer = new byte[2];
-		i2c.read(registerAddress, 2, buffer);
-		return Byte.toUnsignedInt(buffer[0]) * 255 + Byte.toUnsignedInt(buffer[1]);
-	}
-
-	public void write8(int registerAddress, int data) {
-		i2c.write(registerAddress, data);
-	}
-
-	public void setInterrupt(boolean i) {
-		int r = read8(TCS34725_ENABLE);
-		if (i) {
-			r |= TCS34725_ENABLE_AIEN;
-		} else {
-			r &= ~TCS34725_ENABLE_AIEN;
-		}
-		write8(TCS34725_ENABLE, r);
+		return rgb;
 	}
 
 	/**
@@ -173,34 +129,14 @@ public class MappedI2C implements Loggable, Updatable {
 	 */
 	@Override
 	public void update() {
-		time++;
-		if (time == 0) {
-			setInterrupt(false);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
-		if (time * 20 < 60) { //delay for 60 milliseconds
-			return;
-		}
-		if (time * 20 == 60) {
-			clear = read16(TCS34725_CDATAL);
-			red = read16(TCS34725_RDATAL);
-			green = read16(TCS34725_GDATAL);
-			blue = read16(TCS34725_BDATAL);
-		}
-		if (time * 20 < 60 + 50) { //delay for 50 milliseconds
-			return;
-		}
-		if (time * 20 == 60 + 60) {
-			int sum = clear;
-			float r, g, b;
-			r = red;
-			r /= sum;
-			g = green; g /= sum;
-			b = blue; b /= sum;
-			r *= 256; g *= 256; b *= 256;
-			cachedRed = (int) r;
-			cachedGreen = (int) g;
-			cachedBlue = (int) b;
-			time = -1;
-		}
+		int[] rgb = readRGB();
+		cachedRed = rgb[0];
+		cachedGreen = rgb[1];
+		cachedBlue = rgb[2];
 	}
 }
